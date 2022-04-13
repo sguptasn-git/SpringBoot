a13aded0566e786c98ccb196d563840c1832dbb1
Gupta <sgupta@medline.com>
2022-04-13 04:04:35 -0500

    Adding fisrt spring boot application

---
+++ FirstDemoSpringBootApp/src/test/java/com/example/FirstDemoSpringBootApp/FirstDemoSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstDemoSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDemoSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
