7a7b9e35921d3a657d0514d0febc4732e217b820
Gupta <sgupta@medline.com>
2022-04-13 05:27:44 -0500

    sping boot rest service

---
+++ FirstDemoSpringBootRestApp/src/test/java/com/example/restservice/FirstDemoSpringBootRestApp/FirstDemoSpringBootRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservice.FirstDemoSpringBootRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDemoSpringBootRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
